6ea3610da038cbfe8fc3a42701abcdd1f421863e
imp-1903 <imperator.1903.petrushka@gmail.com>
2018-10-16 10:01:09 +0300

    Add multimedia))

--- src/views/InfoManage.vue
+++ src/views/InfoManage.vue
@@ -1,51 +1,58 @@
 <template>
-    <div class="columns">
-        <div class="column box is-10 is-offset-1">
-            <h1 class="title">Керування Інформацією</h1>
-            <form>
+    <div>
+        <div class="columns">
+            <div class="column box is-10 is-offset-1">
+                <h1 class="title">Керування Інформацією</h1>
+                <form>
 
-                <label class="label">Змінити Ім'я</label>
-                <div class="field">
-                    <div class="control">
-                        <input class="input" type="text" placeholder="Ім'я">
+                    <label class="label" for="name">Змінити Ім'я</label>
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="text" id="name" placeholder="Ім'я">
+                        </div>
                     </div>
-                </div>
 
-                <label class="label">Змінити Прізвище</label>
-                <div class="field">
-                    <div class="control ">
-                        <input class="input" type="text" placeholder="Прізвище">
+                    <label class="label" for="surname">Змінити Прізвище</label>
+                    <div class="field">
+                        <div class="control ">
+                            <input class="input" type="text" id="surname" placeholder="Прізвище">
+                        </div>
                     </div>
-                </div>
 
-                <label class="label">Змінити Пароль</label>
-                <div class="field">
-                    <div class="control">
-                        <input class="input" type="password" placeholder="Старий пароль">
+                    <label class="label">Змінити Пароль</label>
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="password" placeholder="Старий пароль">
+                        </div>
                     </div>
-                </div>
 
-                <div class="field">
-                    <div class="control">
-                        <input class="input" type="password" placeholder="Новий пароль">
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="password" placeholder="Новий пароль">
+                        </div>
                     </div>
-                </div>
 
-                <div class="field">
-                    <div class="control">
-                        <input class="input" type="password" placeholder="Повторіть новий пароль">
+                    <div class="field">
+                        <div class="control">
+                            <input class="input" type="password" placeholder="Повторіть новий пароль">
+                        </div>
                     </div>
-                </div>
 
-                <div class="field is-grouped">
-                    <div class="control">
-                        <button class="button is-link">Змінити данні</button>
+                    <div class="field is-grouped">
+                        <div class="control">
+                            <button class="button is-link">Змінити данні</button>
+                        </div>
+                        <div class="control">
+                            <input class="button is-text" type="reset" value="Скинути">
+                        </div>
                     </div>
-                    <div class="control">
-                        <input class="button is-text" type="reset" value="Скинути">
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
+        </div>
+        <div class="columns">
+            <div class="column box is-10 is-offset-1">
+                Lol
+            </div>
         </div>
     </div>
 </template>
@@ -53,3 +60,9 @@
 <script>
   export default {}
 </script>
+
+<style>
+    .offset {
+        width: 50px;
+    }
+</style>
