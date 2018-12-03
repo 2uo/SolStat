2cc790330feeb856a72938e8ff43a85de4540f35
Grig Betsan <griggors@gmail.com>
2018-12-04 01:10:20 +0200

    Fixed misspellings in params and data retrieving

--- src/views/Statistic.vue
+++ src/views/Statistic.vue
@@ -2,11 +2,11 @@
     <div class="columns">
         <div class="column box is-10 is-offset-1">
             <h1 class="title">Статистика роботи SPP:</h1>
-            <p>Виробіток за весь час: {{this.stats.statistic.allTime}} kW/h</p>
+            <p>Виробіток за весь час: {{this.stats.statistics.total}} kW/h</p>
             <hr>
-            <p>Середній щомісячний виробіток: {{this.stats.statistic.month}} kW/h</p>
-            <p>Середній щоденний виробіток: {{this.stats.statistic.day}} kW/h</p>
-            <p>Середній щогодинний виробіток: {{this.stats.statistic.hour}} kW/h</p>
+            <p>Середній щомісячний виробіток: {{this.stats.statistics.avgMonth}} kW/h</p>
+            <p>Середній щоденний виробіток: {{this.stats.statistics.avgDay}} kW/h</p>
+            <p>Середній щогодинний виробіток: {{this.stats.statistics.avgHour}} kW/h</p>
             <hr>
             <div class="columns">
                 <div class="column datepicker-container">
@@ -17,7 +17,7 @@
                                 :max-date="maxDate"
                                 :month-names="monthNames"
                                 :day-names="dayNames"
-                                v-model="request.before_date"
+                                v-model="request.after_date"
                                 inline>
                         </b-datepicker>
                     </b-field>
@@ -31,7 +31,7 @@
                                 :max-date="maxDate"
                                 :month-names="monthNames"
                                 :day-names="dayNames"
-                                v-model="request.after_date"
+                                v-model="request.before_date"
                                 inline>
                         </b-datepicker>
                     </b-field>
@@ -63,76 +63,79 @@
 </template>
 
 <script>
-    import LineChart from './LineChart.js'
+  import LineChart from './LineChart.js'
 
-    export default {
-        components: {
-            LineChart
+  export default {
+    components: {
+      LineChart
+    },
+    data() {
+      return {
+        stats: {
+          data: [],
+          range: [],
+          statistics: {
+            avgHour: '',
+            avgDay: '',
+            avgMonth: '',
+            total: ''
+          }
         },
-        data() {
-            return {
-                stats: {
-                    data: [],
-                    range: [],
-                    statistic: {
-                        avgHour: '',
-                        avgDay: '',
-                        avgMonth: '',
-                        allTime: ''
-                    }
-                },
-                dataChart: {
-                    labels: [],
-                    datasets: [
-                        {
-                            label: 'Щомісячний виробіток (kW/h)',
-                            borderWidth: 2,
-                            borderColor: '#00908E',
-                            pointBorderColor: '#007272',
-                            pointBackgroundColor: '#1fcac8',
-                            backgroundColor: '#00b5b1',
-                            data: []
-                        }
-                    ]
-                },
-                minDate: undefined,
-                maxDate: new Date(),
-                monthNames: ["Січень", "Лютий", "Березень", "Квітень",
-                    "Травень", "Червень", "Липень", "Серпень", "Вересень",
-                    "Жовтень", "Листопад", "Грудень"],
-                dayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
-                request: {
-                    before_date: undefined,
-                    after_date: undefined,
-                    period: 'hour'
-                },
-                firstDayOfWeek: '1'
+        dataChart: {
+          labels: [],
+          datasets: [
+            {
+              label: 'Щомісячний виробіток (kW/h)',
+              borderWidth: 2,
+              borderColor: '#00908E',
+              pointBorderColor: '#007272',
+              pointBackgroundColor: '#1fcac8',
+              backgroundColor: '#00b5b1',
+              data: []
             }
+          ]
         },
-        beforeMount() {
-            this.axios.get('/stats', {
-                before_date: 'start',
-                after_date: 'start',
-                period: 'hour'
-            }).then(resp => {
-                this.stats = resp.data;
-            }).catch(err => {
-                console.log(err)
-            })
+        minDate: undefined,
+        maxDate: new Date(),
+        monthNames: ["Січень", "Лютий", "Березень", "Квітень",
+          "Травень", "Червень", "Липень", "Серпень", "Вересень",
+          "Жовтень", "Листопад", "Грудень"],
+        dayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
+        request: {
+          before_date: undefined,
+          after_date: undefined,
+          period: 'hour'
         },
-        methods: {
-            resetData() {
-                this.axios.get('/stats', this.request).then(resp => {
-                    this.stats = resp.data;
-
-                    this.dataChart.labels = this.stats.data[0]
-                    this.dataChart.datasets[0].data = this.stats.data[1]
-                });
-
-                this.$children[1].renderChart(this.dataChart)
-            }
+        firstDayOfWeek: '1'
+      }
+    },
+    beforeMount() {
+      this.axios.get('/stats', {
+        params: {
+          before_date: 'start',
+          after_date: 'start',
+          period: 'hour'
         }
+      }).then(resp => {
+        this.stats = resp.data;
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    methods: {
+      resetData() {
+        this.axios.get('/stats', {
+          params: this.request
+        }).then(resp => {
+          this.stats = resp.data;
+          this.dataChart.labels = this.stats.data.dates
+          this.dataChart.datasets[0].data = this.stats.data.amounts
+        });
+
+        this.$children[3].renderChart(this.dataChart)
+      }
     }
+  }
 </script>
 
 <style>
