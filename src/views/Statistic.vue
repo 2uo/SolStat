33846cc8e5b00516685eb6af68a974014a986774
Grigoriy Betsan <griggors@gmail.com>
2018-12-13 18:19:28 +0200

    Fix graphs

--- src/views/Statistic.vue
+++ src/views/Statistic.vue
@@ -2,11 +2,11 @@
     <div class="columns">
         <div class="column box is-10 is-offset-1">
             <h1 class="title">Статистика роботи SPP:</h1>
-            <p>Виробіток за весь час: {{this.stats.statistics.total}} kW/h</p>
+            <p>Виробіток за весь час: {{this.stats.statistics.total.toPrecision(6)}} kW/h</p>
             <hr>
-            <p>Середній щомісячний виробіток: {{this.stats.statistics.avgMonth}} kW/h</p>
-            <p>Середній щоденний виробіток: {{this.stats.statistics.avgDay}} kW/h</p>
-            <p>Середній щогодинний виробіток: {{this.stats.statistics.avgHour}} kW/h</p>
+            <p>Середній щомісячний виробіток: {{this.stats.statistics.avgMonth.toPrecision(5)}} kW/h</p>
+            <p>Середній щоденний виробіток: {{this.stats.statistics.avgDay.toPrecision(4) }} kW/h</p>
+            <p>Середній щогодинний виробіток: {{this.stats.statistics.avgHour.toPrecision(4) }} kW/h</p>
             <hr>
             <div class="columns">
                 <div class="column datepicker-container">
@@ -17,7 +17,7 @@
                                 :max-date="maxDate"
                                 :month-names="monthNames"
                                 :day-names="dayNames"
-                                v-model="request.before_date"
+                                v-model="request.after_date"
                                 inline>
                         </b-datepicker>
                     </b-field>
@@ -31,7 +31,7 @@
                                 :max-date="maxDate"
                                 :month-names="monthNames"
                                 :day-names="dayNames"
-                                v-model="request.after_date"
+                                v-model="request.before_date"
                                 inline>
                         </b-datepicker>
                     </b-field>
@@ -63,101 +63,95 @@
 </template>
 
 <script>
-    import LineChart from './LineChart.js'
+  import LineChart from './LineChart.js'
 
-    export default {
-        components: {
-            LineChart
+  export default {
+    components: {
+      LineChart
+    },
+    data() {
+      return {
+        stats: {
+          data: [],
+          range: [],
+          statistics: {
+            avgHour: '',
+            avgDay: '',
+            avgMonth: '',
+            total: ''
+          }
         },
-        data() {
-            return {
-                stats: {
-                    data: [],
-                    range: [],
-                    statistics: {
-                        avgHour: '',
-                        avgDay: '',
-                        avgMonth: '',
-                        total: ''
-                    }
-                },
-                dataChart: {
-                    labels: [],
-                    datasets: [
-                        {
-                            label: 'Щомісячний виробіток (kW/h)',
-                            borderWidth: 2,
-                            borderColor: '#00908E',
-                            pointBorderColor: '#007272',
-                            pointBackgroundColor: '#1fcac8',
-                            backgroundColor: '#00b5b1',
-                            data: []
-                        }
-                    ]
-                },
-                minDate: undefined,
-                maxDate: new Date(),
-                monthNames: ["Січень", "Лютий", "Березень", "Квітень",
-                    "Травень", "Червень", "Липень", "Серпень", "Вересень",
-                    "Жовтень", "Листопад", "Грудень"],
-                dayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
-                request: {
-                    before_date: undefined,
-                    after_date: undefined,
-                    period: 'hour'
-                }
-            }
-        },
-        beforeMount() {
-            this.axios.get('/stats', {
-                params: {
-                    before_date: 'start',
-                    after_date: 'start',
-                    period: 'hour'
-                }
-            }).then(resp => {
-                var dt = {
-                    first: '',
-                    last: ''
-                };
-                dt.first = resp.data.range.first_date.split(/[: T-]/).map(parseFloat);
-                dt.last = resp.data.range.last_date.split(/[: T-]/).map(parseFloat);
-                this.minDate = new Date(dt.first[0], dt.first[1] - 1, dt.first[2], dt.first[3] || 0, dt.first[4] || 0, dt.first[5] || 0, 0);
-                this.maxDate = new Date(dt.last[0], dt.last[1] - 1, dt.last[2], dt.last[3] || 0, dt.last[4] || 0, dt.last[5] || 0, 0);
-                this.request.before_date = new Date(dt.first[0], dt.first[1] - 1, dt.first[2] + 1, dt.first[3] || 0, dt.first[4] || 0, dt.first[5] || 0, 0);
-                this.request.after_date = this.maxDate
-                this.stats = resp.data;
-
-                console.log(resp.data)
-
-            }).catch(err => {
-
-                console.log('Request error: ' + err)
-
-            })
-        },
-        mounted() {
-            this.resetData();
-        },
-        methods: {
-            resetData() {
-                console.log('start reseting data')
-                this.axios.get('/stats', {
-                    params: this.request
-                }).then(resp => {
-                    this.stats = resp.data;
-                    this.dataChart.labels = this.stats.data.dates
-                    this.dataChart.datasets[0].data = this.stats.data.amounts
-                    console.log('reseting data was success')
-                }).catch(err => {
-                    console.log('reseting data returned error: ')
-                    console.log(err)
-                });
-
-                this.$children[3].renderChart(this.dataChart)
-            }
+        dataChart: null,
+        minDate: undefined,
+        maxDate: new Date(),
+        monthNames: ["Січень", "Лютий", "Березень", "Квітень",
+          "Травень", "Червень", "Липень", "Серпень", "Вересень",
+          "Жовтень", "Листопад", "Грудень"],
+        dayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
+        request: {
+          before_date: undefined,
+          after_date: undefined,
+          period: 'hour'
         }
+      }
+    },
+    beforeMount() {
+      this.axios.get('/stats', {
+        params: {
+          before_date: 'start',
+          after_date: 'start',
+          period: 'hour'
+        }
+      }).then(resp => {
+        var dt = {
+          first: '',
+          last: ''
+        };
+        dt.first = resp.data.range.first_date.split(/[: T-]/).map(parseFloat);
+        dt.last = resp.data.range.last_date.split(/[: T-]/).map(parseFloat);
+        this.minDate = new Date(dt.first[0], dt.first[1] - 1, dt.first[2], dt.first[3] || 0, dt.first[4] || 0, dt.first[5] || 0, 0);
+        this.maxDate = new Date(dt.last[0], dt.last[1] - 1, dt.last[2], dt.last[3] || 0, dt.last[4] || 0, dt.last[5] || 0, 0);
+        this.request.after_date = new Date(dt.first[0], dt.first[1] - 1, dt.first[2] + 1, dt.first[3] || 0, dt.first[4] || 0, dt.first[5] || 0, 0);
+        this.request.before_date = this.maxDate
+        this.stats = resp.data;
+
+        console.log(resp.data)
+
+      }).catch(err => {
+        console.log('Request error: ' + err)
+      })
+    },
+    mounted() {
+      this.resetData();
+    },
+    methods: {
+      resetData() {
+        this.axios.get('/stats', {
+          params: this.request
+        }).then(resp => {
+          this.stats = resp.data;
+          this.dataChart = {
+            labels: this.stats.data.dates,
+            datasets: [
+              {
+                label: 'Періодичний виробіток (kW/h)',
+                borderWidth: 2,
+                borderColor: '#00908E',
+                pointBorderColor: '#007272',
+                pointBackgroundColor: '#1fcac8',
+                backgroundColor: '#00b5b1',
+                data: this.stats.data.amounts
+              }
+            ]
+          }
+          console.log(resp.data)
+        }).catch(err => {
+          console.log('reseting data returned error: ')
+          console.log(err)
+        });
+      }
     }
+  }
 </script>
 
 <style>
