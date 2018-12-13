a13ab9ab951701cc7426d1fb2138d65c1de4086e
imp-1903 <imperator.1903.petrushka@gmail.com>
2018-12-14 00:20:52 +0200

    add description and update tutorial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,11 +14,7 @@
             <router-link to="/logged" v-if="logged">сторінку реєстрації</router-link>
             <router-link to="/signup" v-else>сторінку реєстрації</router-link>
             і заповніть коротку анкету, відповівши на
-            декілька дуже простих запитань. Наші менеджери звяжуться з Вами і Ви зможете приступити до роботи з нами
-            одразу після
-            <router-link to="/price" v-if="logged">внесення оплати</router-link>
-            <router-link to="/login" v-else>внесення оплати</router-link>
-            за користування нашим продуктом.
+            декілька дуже простих запитань.
         </p>
         <hr>
         <iframe width="560"
